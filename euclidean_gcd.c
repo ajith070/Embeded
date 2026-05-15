c6100a03d498c9304571bf6bdbbe9609971bd89d
ajith070 <47020173+ajith070@users.noreply.github.com>
2026-05-15 22:12:39 +0400

    Add Euclidean GCD implementation in C

---
+++ euclidean_gcd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Recursive function to find GCD using Euclidean algorithm
+int findGCD(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return findGCD(b, a % b);
+}
+
+int main() {
+    int num1, num2;
+    
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &num1, &num2);
+    
+    printf("GCD of %d and %d is: %d\n", num1, num2, findGCD(num1, num2));
+    
+    return 0;
+}
